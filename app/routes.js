db1875c7499ffbcf27782a7db914e758d4cfd82e
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2021-01-05 14:12:04 +0000

    Send SMS messages

--- app/routes.js
+++ app/routes.js
@@ -7,21 +7,33 @@ const NotifyClient = require('notifications-node-client').NotifyClient,
 // Add your routes here - above the module.exports line
 
 router.post('/confirmation', (req, res) => {
-  notify.sendEmail(
-    // this long string is the template ID, copy it from the template
-    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-    // in your code.
-    '349ec5ea-b7b5-4401-8a6e-3293aef13818',
-    // `emailAddress` here needs to match the name of the form field in
-    // your HTML page
-    req.session.data.emailAddress,
-    {
-      personalisation: {
-        'country': req.session.data.country,
-        'first name': req.session.data.firstName
-      }
+  const { emailAddress, phoneNumber } = req.session.data;
+  const options = {
+    personalisation: {
+      'country': req.session.data.country,
+      'first name': req.session.data.firstName
     }
-  );
+  };
+
+  if (emailAddress) {
+    notify.sendEmail(
+      // this long string is the template ID, copy it from the template
+      // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+      // in your code.
+      '349ec5ea-b7b5-4401-8a6e-3293aef13818',
+      // `emailAddress` here needs to match the name of the form field in
+      // your HTML page
+      req.session.data.emailAddress,
+      options
+    );
+  }
+  if (phoneNumber) {
+    notify.sendSms(
+      'f8ff65b3-d33b-4649-bfe4-f19500f25c4a',
+      req.session.data.phoneNumber,
+      options
+    )
+  }
 
   // This is the URL the users will be redirected to once the email
   // has been sent
