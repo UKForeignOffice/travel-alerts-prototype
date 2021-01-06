1b5322fc8e2fd34788d1598dd5df9eeb8c345496
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2021-01-06 12:22:32 +0000

    Extra page for british nationals; copy updates

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -23,5 +23,6 @@ const fullCountryNames = Object.values(fullCountryList).map(country => country.i
 
 module.exports = {
   fullCountryNames,
-  availableCountries: fullCountryNames
+  availableCountries: fullCountryNames,
+  countries: ['Thailand']
 }
